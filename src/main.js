b74b73daed0b678711fc6a8036775852514a327d
lateo16 <lateofrederick@gmail.com>
2019-07-12 18:56:37 +0000

    imported bootstrap and other npm packages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+/* eslint-disable semi */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/* jshint esversion: 6 */
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import { firestorePlugin } from 'vuefire';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import firebase from 'firebase';
+
+Vue.use(firestorePlugin);
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +22,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
