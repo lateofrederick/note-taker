1cd38e53226f2a7fc19b848cc7b00eae6befcbc3
lateo16 <lateofrederick@gmail.com>
2019-07-12 18:32:15 +0000

    imported some components and implemented nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div id="app">
     <Navigation/>
@@ -21,29 +20,3 @@ export default {
 <style>
 
 </style>
-||||||| merged common ancestors
-=======
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
->>>>>>> cc41daa3bb1e5de003e4434ff1b0cd37a389debb
