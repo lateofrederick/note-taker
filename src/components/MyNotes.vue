539b507bfecf105a7358fba94ca1a0935dc241da
lateo16 <lateofrederick@gmail.com>
2019-07-29 15:36:34 +0000

    Added a pop up notification to specify whether to delete a note

--- src/components/MyNotes.vue
+++ src/components/MyNotes.vue
@@ -3,11 +3,11 @@
     <h1>Notes</h1>
     <div v-for="(anote,idx) in notes" :key="idx" class="card  mb-3" style="width:30rem;">
       <div class="card-body">
-        <h5 class="card-title">{{anote.note.title}}</h5>
-        <p class="card-text">{{anote.note.content}}</p>
-        <p class="card-text">{{anote.date}}</p>
+        <h5 class="card-title">{{anote.noteinfo.data().note.title}}</h5>
+        <p class="card-text">{{anote.noteinfo.data().note.content}}</p>
+        <p class="card-text">{{anote.noteinfo.data().date}}</p>
         <div class="text-right">
-          <p class="btn text-white" style="background-color:#64c746">Delete</p>
+          <p @click="deletenote(anote.noteinfo.id)" class="btn text-white" style="background-color:#64c746">Delete</p>
         </div>
       </div>
     </div>
@@ -32,12 +32,23 @@ export default {
     }
   },
   methods: {
+    // implementing a method to delete note
+    deletenote(id) {
+      if(confirm("Do you want to delete note")){
+        db.collection('notes').doc(id).delete().then(function(data){
+          window.location = window.location
+        })
+        console.log("note removed");
+     }else {
+       // do nothing
+     }
+      }
   },
   mounted() {
     //Retrieve notes from firebase after mounted instance
      db.collection('notes').get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        this.notes.push(doc.data());
+        this.notes.push({noteinfo:doc});
         })
       })
     }
